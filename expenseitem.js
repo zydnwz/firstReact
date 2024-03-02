54a70e43f94a94f64c78f8814a67329c7d74af84
zydnwz <zaidnawaz@2018@gmail.com>
2024-03-02 06:33:47 +0530

    first commit

---
+++ expenseitem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <ul>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseItem;
